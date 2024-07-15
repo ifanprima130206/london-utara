19f1219aa2ed59c304cbfe0c5ffc7ea57063bcdb
Yoga Pangestu <fanproject13@gmail.com>
2024-07-15 10:45:48 +0700

    Deploy Test on Railway

--- app/Models/Back/User.php
+++ app/Models/Back/User.php
@@ -3,14 +3,13 @@
 namespace App\Models\Back;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, SoftDeletes;
+    use SoftDeletes;
 
     protected $table = 'users';
 
