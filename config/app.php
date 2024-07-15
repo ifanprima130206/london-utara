b51d50030d2a05980ba393b8cb011f5066c9ef4e
Yoga Pangestu <fanproject13@gmail.com>
2024-07-15 11:05:57 +0700

    Deploy Test on Railway

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'London Utara'),
+    'name' => env('APP_NAME', 'London-Utara'),
 
     /*
     |--------------------------------------------------------------------------
